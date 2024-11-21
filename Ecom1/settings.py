14bfa191dad989dfc282c5f8a020ff7a563b2189
Abhishek M Nair <102518312+abhishekmnair81@users.noreply.github.com>
2024-11-21 22:35:32 +0530

    Update settings.py

--- Ecom1/settings.py
+++ Ecom1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=b@sk4f56tryz2(zba7zlwky4@x^(tr5ot03k0tg5s(fb%#=-2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
